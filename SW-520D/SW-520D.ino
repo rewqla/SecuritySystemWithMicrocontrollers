78b665ac592a3444acad2e4f06e40c2e4dfec968
rewqla <desares526@gmail.com>
2023-11-21 16:50:54 +0200

    Add a SW-520D module

---
+++ SW-520D/SW-520D.ino
@@ -0,0 +1,18 @@
+const int vibrationPin = 6; // Connect the SW-520D signal pin to digital pin 2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(vibrationPin, INPUT);
+}
+
+void loop() {
+  int vibrationValue = digitalRead(vibrationPin);
+
+  if (vibrationValue == HIGH) {
+    Serial.println("Vibration detected!");
+  } else {
+    Serial.println("No vibration");
+  }
+
+  delay(200); // Adjust the delay as needed
+}
