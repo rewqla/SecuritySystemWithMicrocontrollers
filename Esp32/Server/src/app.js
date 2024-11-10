7863c6dae551a33f8cbe083e8b3270d5c8e2deb2
rewqla <desares526@gmail.com>
2024-11-10 11:27:49 +0200

    Added register endpoint

--- Esp32/Server/src/app.js
+++ Esp32/Server/src/app.js
@@ -6,11 +6,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({ extended: true }));
+
+const users = [
+    { username: 'admin', password: '1234' }
+];
 
 app.get('/', (req, res) => {
     res.render('login');
+});
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
 app.get('/cabinet', (req, res) => {
@@ -19,12 +25,25 @@ app.get('/cabinet', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-
-    if (username === 'admin' && password === 'password') {
+    console.log(users)
+    const user = users.find(user => user.username === username && user.password === password);
+    if (user) {
         res.redirect('/cabinet');
     } else {
         res.send('Invalid credentials');
     }
+});
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.send('Username already exists. Please choose another one.');
+    }
+
+    users.push({ username, password });
+    res.send('Registration successful! You can now <a href="/">login</a>.');
 });
 
 app.get('/api/configuration', (req, res) => {
