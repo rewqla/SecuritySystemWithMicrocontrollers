7cdb6db1410659b89436025ee49d3f04658d2bff
rewqla <desares526@gmail.com>
2024-11-10 09:53:24 +0200

    Added new endpoints

--- Esp32/Server/src/app.js
+++ Esp32/Server/src/app.js
@@ -1,36 +1,40 @@
-// app.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
-// Serve static files (like CSS) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware to handle POST data
 app.use(express.urlencoded({ extended: true }));
 
-// Route to render login page
 app.get('/', (req, res) => {
     res.render('login');
 });
 
-// Route to handle form submission
+app.get('/cabinet', (req, res) => {
+    res.render('cabinet');
+});
+
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    // Simple authentication logic (you can replace this with real authentication)
     if (username === 'admin' && password === 'password') {
-        res.send('Login successful');
+        res.redirect('/cabinet');
     } else {
         res.send('Invalid credentials');
     }
 });
 
-// Start the server
+app.get('/api/configuration', (req, res) => {
+    const configuration = {
+        text: "config text"
+    };
+
+    res.json(configuration);
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
